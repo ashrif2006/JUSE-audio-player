ec5db1161adbca3b4491d151cdaaf343eb27072f
ShahdMohammed <shahdelhewehi@gmail.com>
2025-10-17 02:08:24 +0300

    Fix formatting and add missing semicolon

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,13 +1,36 @@
-﻿#pragma once
+ #pragma once
 
 #include <JuceHeader.h>
+
+// رقم 9 - كلاس الشكل الموجي
+class WaveformDisplay : public juce::Component, public juce::ChangeListener
+{
+public:
+    WaveformDisplay();
+    ~WaveformDisplay() override;
+
+    void paint(juce::Graphics& g) override;
+    void resized() override;
+
+    void loadFile(const juce::File& file);
+    void setPosition(double pos);
+
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
+
+private:
+    juce::AudioFormatManager formatManager;
+    juce::AudioThumbnailCache thumbnailCache;
+    juce::AudioThumbnail audioThumbnail;
+    double position = 0.0;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(WaveformDisplay)
+};
 
 class MainComponent : public juce::AudioAppComponent,
     public juce::Button::Listener,
     public juce::Slider::Listener,
-    public juce::ListBoxModel
-    public juce::Timer
-
+    public juce::ListBoxModel,
+    public juce::Timer  // إضافة فاصلة منقوطة
 {
 public:
     MainComponent();
@@ -32,9 +55,9 @@ public:
     int getNumRows() override;
     void paintListBoxItem(int rowNumber, juce::Graphics& g, int width, int height, bool rowIsSelected) override;
     void selectedRowsChanged(int lastRowSelected) override;
+    
     // رقم 9 - وظائف التايمر
     void timerCallback() override;
-
 
 private:
     void loadFile(const juce::String& path);
@@ -54,12 +77,8 @@ private:
     juce::Slider volumeSlider;
     juce::Component waveformPlaceholder;
 
-
     juce::ListBox playlistBox;
-
     juce::Label metadataLabel;
-
-    std::unique_ptr<juce::FileChooser> fileChooser;
 
     juce::StringArray playlistFiles; // قائمة ملفات المسارات
     int currentFileIndex{ -1 };
@@ -67,42 +86,19 @@ private:
     bool isMuted{ false };
     bool isLooping{ false };
     float lastVolume{ 0.5f };
-         // رقم 6 - عناصر السرعة
- juce::Slider speedSlider;
- juce::Label speedLabel;
- double getSpeedFromIndex(int index);
- void updateSpeedLabel();
 
- // رقم 9 - عناصر الشكل الموجي والموضع
- WaveformDisplay waveformDisplay;
- juce::Slider positionSlider;
- juce::Label currentTimeLabel;
- juce::Label totalTimeLabel;
- juce::String formatTime(double seconds);
+    // رقم 6 - عناصر السرعة
+    juce::Slider speedSlider;
+    juce::Label speedLabel;
+    double getSpeedFromIndex(int index);
+    void updateSpeedLabel();
 
+    // رقم 9 - عناصر الشكل الموجي والموضع
+    WaveformDisplay waveformDisplay;
+    juce::Slider positionSlider;
+    juce::Label currentTimeLabel;
+    juce::Label totalTimeLabel;
+    juce::String formatTime(double seconds);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
-};
-// رقم 9 - كلاس الشكل الموجي
-class WaveformDisplay : public juce::Component, public juce::ChangeListener
-{
-public:
-    WaveformDisplay();
-    ~WaveformDisplay() override;
-
-    void paint(juce::Graphics& g) override;
-    void resized() override;
-
-    void loadFile(const juce::File& file);
-    void setPosition(double pos);
-
-    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
-
-private:
-    juce::AudioFormatManager formatManager;
-    juce::AudioThumbnailCache thumbnailCache;
-    juce::AudioThumbnail audioThumbnail;
-    double position = 0.0;
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(WaveformDisplay)
 };
